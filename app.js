acd76e6b18c0ca6c7b43e49789dc8df4494671bd
zencodingllc <zencodingllc@gmail.com>
2026-04-22 18:59:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const http = require('http');
 
-const port = 3000;
-
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end("DevOps App is LIVE 🚀");
-}).listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
-});
+}).listen(3000, '0.0.0.0');
